3682bb0f2abdf7b850e77fca0e1a9b178d1dda33
Jose Catarino <jose.catarino@factioninc.com>
2023-05-07 17:42:26 -0600

    v1/tasks POST working

--- app/backend/libs/utils.py
+++ app/backend/libs/utils.py
@@ -1,6 +1,18 @@
 """General utility functions and classes."""
+import json
 import logging
 import sys
+
+
+class JsonFormatter(logging.Formatter):
+    """JSON log formatter"""
+
+    def format(self, record):
+        if isinstance(record.msg, (dict, list)):
+            # line below pretty prints json
+            record.msg = json.dumps(record.msg, indent=4)
+            # pprint.pprint(record.msg)
+        return super().format(record)
 
 
 class LoggingService:
@@ -20,7 +32,7 @@ class LoggingService:
         stream_handler = logging.StreamHandler(sys.stdout)
 
         # Set the log format
-        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        formatter = JsonFormatter("%(asctime)s - %(levelname)s - %(message)s")
         stream_handler.setFormatter(formatter)
 
         # Add the stream handler to the logger
